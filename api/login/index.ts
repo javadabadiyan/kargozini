faac0f8c72f9eb8ca67706aaa6c7ffc306dae7b9
javadabadiyan <abadianjavad@gmail.com>
2025-08-20 09:25:46 +0330

    Delete api/login/index.ts

--- api/login/index.ts
+++
@@ -1,3 +0,0 @@
-// This API endpoint is obsolete.
-// Its functionality has been merged into `/api/users?module=admin&action=login`.
-// This file MUST BE DELETED from the project repository to solve the Vercel deployment limit error.
