40790143fbd13e90e312719e4c98ff774c6a4564
javadabadiyan <abadianjavad@gmail.com>
2025-08-20 09:24:25 +0330

    Delete api/backup/index.ts

--- api/backup/index.ts
+++
@@ -1,3 +0,0 @@
-// This API endpoint is obsolete.
-// Its functionality has been merged into `/api/users?module=admin&action=backup`.
-// This file MUST BE DELETED from the project repository to solve the Vercel deployment limit error.
