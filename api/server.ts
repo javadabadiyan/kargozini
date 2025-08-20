2bd28587eb23b2c8e46cb327b1f5890aface378b
javadabadiyan <abadianjavad@gmail.com>
2025-08-20 09:27:50 +0330

    Delete api/server.ts

--- api/server.ts
+++
@@ -1,4 +0,0 @@
-// This file is obsolete. The local development server has been moved to the project root (`/server.ts`).
-//
-// IMPORTANT: This file MUST BE DELETED from the project repository to solve the
-// Vercel deployment limit error and avoid confusion with the correct local server file.
