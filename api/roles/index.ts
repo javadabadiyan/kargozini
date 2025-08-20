36072eb41d1994081c136713493d72e5b7fac216
javadabadiyan <abadianjavad@gmail.com>
2025-08-20 07:09:52 +0330

    Delete api/roles/index.ts

--- api/roles/index.ts
+++
@@ -1,3 +0,0 @@
-// This API endpoint is obsolete.
-// Role management has been replaced by the permission system in the main API endpoint.
-// This file MUST BE DELETED from the project repository to solve the Vercel deployment limit error.
