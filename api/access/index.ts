77be3a1fffc692a0f56776232386a29e90f7aa9a
javadabadiyan <abadianjavad@gmail.com>
2025-08-20 09:23:24 +0330

    Delete api/access/index.ts

--- api/access/index.ts
+++
@@ -1,3 +0,0 @@
-// This API endpoint is obsolete.
-// Access control is now handled by the permission system in the main API endpoint.
-// This file MUST BE DELETED from the project repository to solve the Vercel deployment limit error.
