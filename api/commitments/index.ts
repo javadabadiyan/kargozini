c897bcf760ca95c566693749108257702d1f58e1
javadabadiyan <abadianjavad@gmail.com>
2025-08-20 07:08:08 +0330

    Delete api/commitments/index.ts

--- api/commitments/index.ts
+++
@@ -1,3 +0,0 @@
-// This API endpoint is obsolete.
-// Its functionality has been merged into `/api/users?module=personnel&type=commitments`.
-// This file MUST BE DELETED from the project repository to solve the Vercel deployment limit error.
