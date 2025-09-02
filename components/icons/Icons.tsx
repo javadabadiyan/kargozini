9ba41b0c59dfc9d573d84b1a42fb15bb33df5c8d
javadabadiyan <abadianjavad@gmail.com>
2025-09-02 06:22:43 +0330

    refactor: Remove unused ClockIcon
    The ClockIcon was no longer being used in the LogCommutePage component. This commit removes the unused icon definition to clean up the codebase.

--- components/icons/Icons.tsx
+++ components/icons/Icons.tsx
@@ -113,9 +113,3 @@ export const XIcon: React.FC<IconProps> = (props) => (
     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
   </svg>
 );
-
-export const ClockIcon: React.FC<IconProps> = (props) => (
-  <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-  </svg>
-);
